282fd432588e5eecf9ba8426f4e9d84d92ee3373
Nasusann <kawarayako@cy-com.co.jp>
2025-06-06 10:39:37 +0900

    コメント追加

--- src/main/java/com/example/music_management/viewmodel/AlbumViewModel.java
+++ src/main/java/com/example/music_management/viewmodel/AlbumViewModel.java
@@ -4,12 +4,19 @@ import java.time.LocalDate;
 
 import lombok.Data;
 
+// アルバムの情報を保持するためのクラスです
+// Lombokのアノテーションで、ゲッター、セッター、toStringなどを自動生成
 @Data
 public class AlbumViewModel {
+    // アルバムのIDを保持するフィールドです
     private long albumId;
+    // アルバムのタイトルを保持するフィールドです
     private String title;
+    // アルバムのアーティスト名を保持するフィールドです
     private String artist;
+    // アルバムのリリース日を保持するフィールドです
     private LocalDate releaseDate;
+    // アルバムに含まれる音楽の数を保持するフィールドです
     private int musicCount;
 
 }
--- src/main/java/com/example/music_management/viewmodel/MusicViewModel.java
+++ src/main/java/com/example/music_management/viewmodel/MusicViewModel.java
@@ -6,11 +6,18 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
+// 音楽の情報を保持するためのクラスです
+// Lombokのアノテーションで、ゲッター、セッター、toStringなどを自動生成
 @Data
 public class MusicViewModel {
+    // 音楽のIDを保持するフィールドです
     private long musicId;
+     // 音楽のタイトルを保持するフィールドです
     private String title;
+     // 音楽の再生時間を保持するフィールドです
+    // @DateTimeFormatアノテーションで、時間のフォーマットを指定しています
     @DateTimeFormat(pattern = "HH:mm:ss")
     private LocalTime duration;
+    // お気に入りかどうかを示すフラグを保持するフィールドです
     private boolean isFavorite;
 }
